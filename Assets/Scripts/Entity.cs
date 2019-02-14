dd153c96c330e0c60a4ac9c420ba2521cd57e9ce
Bert Haddad <bhaddad5@gmail.com>
2019-02-14 00:59:50 -0800

    And the Bert said: LET THERE BE GRAVITY

---
+++ Assets/Scripts/Entity.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Entity : MonoBehaviour
+{
+	public double MassKg;
+
+	[SerializeField] private Entity parent;
+	[SerializeField] private Vector3 currentSpeed;
+	
+	// Update is called once per frame
+	void Update ()
+	{
+		if (parent != null)
+		{
+			float dist = parent.transform.position.FromTo(transform.position).magnitude;
+
+			//Debug.Log(dist);
+
+			float gravity = (float)(PhysicsHelpers.GravitationalConstant * ((MassKg * parent.MassKg) / (dist * dist)));
+
+			//Debug.Log(gravity);
+
+			Vector3 gravityDirection = transform.position.FromTo(parent.transform.position).normalized;
+
+			Vector3 gravityVector = gravityDirection * gravity;
+
+			//Debug.Log(gravityVector.ToString("f8"));
+
+			currentSpeed = currentSpeed + gravityVector;
+
+			transform.position += currentSpeed;
+		}
+	}
+}
---
+++ Assets/Scripts/PhysicsHelpers.cs
@@ -0,0 +1,9 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public static class PhysicsHelpers
+{
+	//kg m seconds
+	public const double GravitationalConstant = 6.67408e-11;
+}
